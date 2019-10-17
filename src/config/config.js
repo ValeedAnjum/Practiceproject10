cef5bc19437fef1654495b1566a641d6cf5a35f7
ValeedAnjum <32477374+ValeedAnjum@users.noreply.github.com>
2019-10-17 12:16:33 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,30 +3,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 var firebaseConfig = {
-  apiKey: "AIzaSyD_8moDWZBaHzNUZwN7KySCG8sMJpUtB_U",
-  authDomain: "ecommerce-76e5f.firebaseapp.com",
-  databaseURL: "https://ecommerce-76e5f.firebaseio.com",
-  projectId: "ecommerce-76e5f",
-  storageBucket: "ecommerce-76e5f.appspot.com",
-  messagingSenderId: "69359416400",
-  appId: "1:69359416400:web:2fe4e34d85fb808de28d55"
+  apiKey: "XXX",
+  authDomain: "XXX",
+  databaseURL: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export default firebase;
-
-// import firebase from 'firebase';
-// import 'firebase/firestore';
-// import 'firebase/auth';
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCzAxSOEcoNWl07VlKISKoM_OBhu_cP7ck",
-//   authDomain: "ecommerce-e936a.firebaseapp.com",
-//   databaseURL: "https://ecommerce-e936a.firebaseio.com",
-//   projectId: "ecommerce-e936a",
-//   storageBucket: "ecommerce-e936a.appspot.com",
-//   messagingSenderId: "210952281580",
-//   appId: "1:210952281580:web:9c33d4d37179284f1634c9"
-//   };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// export default firebase;
